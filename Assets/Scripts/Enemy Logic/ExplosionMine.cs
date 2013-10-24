a17bfc68b45edecb4aff674fb0a7e09312d97743
Juan73908 <juangmgs@hotmail.com>
2013-10-24 13:39:33 +0200

    Add TagManager and explosions for mines

---
+++ Assets/Scripts/Enemy Logic/ExplosionMine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionMine: MonoBehaviour {
+	
+    public float radius = 2.0f;
+    public float power = 2000.0f;
+    public float upwardsModifier = 1.0f;
+	
+    void OnTriggerEnter(Collider other) {
+		
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+		
+        foreach (Collider hit in colliders) {
+            if (hit.rigidbody)
+                hit.rigidbody.AddExplosionForce(power, explosionPos, radius, upwardsModifier);
+		}
+    }
+}
