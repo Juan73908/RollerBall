16a2d660f4230cb933374b5d90792e3784b7c62f
Juan73908 <juangmgs@hotmail.com>
2013-10-24 09:49:17 +0200

    Add mine animation

---
+++ Assets/Scripts/Enemy Logic/AnimationMine.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationMine : MonoBehaviour {
+	
+	public float rotationSpeed = 45.0f;
+	
+	// 
+	void Update () {
+		transform.Rotate(new Vector3( 0, rotationSpeed * Time.deltaTime, 0));
+	}
+}
