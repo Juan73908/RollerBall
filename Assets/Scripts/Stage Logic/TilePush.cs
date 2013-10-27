f26e5b5c38368a08551a214c43b6cfa884f304d8
Juan73908 <juangmgs@hotmail.com>
2013-10-27 20:14:17 +0100

    Add arrow pushing tile

---
+++ Assets/Scripts/Stage Logic/TilePush.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TilePush : MonoBehaviour {
+
+	public float power = 1.0f;
+	public GameObject player;
+	
+	protected Vector3 dir; 
+	
+	void Awake() {
+		// The direction to push the player
+		dir = GameObject.Find("PushDirection").transform.position - transform.position; 
+	}
+	
+	
+    void OnTriggerStay(Collider other) {
+		
+        if ((other.tag == Tags.player) && (other.rigidbody)){
+			// If the ball is over the tile we push it
+			player.constantForce.force += dir * power * Time.deltaTime;
+		}
+    }
+}
