1ee9feb83331fd728b749a6cdfa41895b8aeb47a
Juan73908 <juangmgs@hotmail.com>
2013-10-28 22:57:39 +0100

    Football and rugbyball, also moveable crates

---
+++ Assets/Scripts/General/LockMobileScreen.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockMobileScreen : MonoBehaviour {
+	
+	public int seconds;
+	
+	void Start () {
+		Screen.sleepTimeout = seconds;
+	}
+}
