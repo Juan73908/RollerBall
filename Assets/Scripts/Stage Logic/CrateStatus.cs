45f4dd9623af042701989794353bcd6fe6affee3
Juan73908 <juangmgs@hotmail.com>
2013-11-04 12:35:01 +0100

    Add level 14, bouncer1x1 and crate prefabs

---
+++ Assets/Scripts/Stage Logic/CrateStatus.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrateStatus : MonoBehaviour {
+	
+	public int destroyOnY = -100;
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < destroyOnY)
+		{
+			// The crate fell, so we delete it
+			Destroy(gameObject);
+		}
+	}
+}
